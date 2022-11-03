d1073d305ec8334e191766009c399c06f863c727
hexinyu <1445182853@qq.com>
2022-11-03 11:37:33 +0800

    'third'

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+const query = require('../utils/mySql');
+const jiami = require('../utils/sha1');
+const getToken = require('../utils/getToken');
+
 // 管理员
-router.post('/reg', function (req, res, next) {
-  res.json({ a: 1 })
+router.post('/reg', async function (req, res, next) {
+  try {
+    const { uid, pwd } = req.body;
+    const users = await query('select * from admins where username=?', [uid]);
+    if (users.length > 0) {
+      res.json({
+        flag: false,
+        msg: '用户名已存在'
+      })
+    } else {
+      await query('insert into admins (username,password,createdAt,updatedAt) values (?,?,now(),now())', [uid, jiami(pwd)])
+      res.json({
+        flag: true,
+        msg: '注册成功'
+      })
+    }
+  } catch (e) {
+    next(e)
+  }
+
 });
 
 router.post('/login', function (req, res, next) {
